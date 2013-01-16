ac694590ac3db2ce5dddde5c715f8cdaae5ca151
Peter Desmet <peter.desmet.work@gmail.com>
2013-01-16 13:53:20 +0100

    Extended basic settings

--- _source/pelicanconf.py
+++ _source/pelicanconf.py
@@ -1,22 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
-AUTHOR = u'Peter Desmet'
-SITENAME = u'My Blog'
-SITEURL = ''
+SITENAME = u"Anderhalv"
+SITEURL = "http://peterdesmet.github.com"
+AUTHOR = u"Peter Desmet"
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = "Europe/Paris"
+DEFAULT_LANG = u"en"
 
-DEFAULT_LANG = u'en'
+OUTPUT_PATH = "../" # One level above the _source folder
+DEFAULT_PAGINATION = False # Verify
+DELETE_OUTPUT_DIRECTORY = False # True doesn't seem to work + Not sure if it would remove _source
+DISPLAY_PAGES_ON_MENU = True
+FEED_ALL_RSS = "feeds/rss.xml" # Verify
+FEEDS_ALL_ATOM = None # Verify
+
+THEME = "tuxlite_tbs"
+
+# Ignore the following work folders during publication
+PAGE_EXCLUDES = (
+)
+ARTICLE_EXCLUDES = (
+	("pages"),
+)
+
+# External services
+DISQUS_SITENAME = "" # Set
+GOOGLE_ANALYTICS = "" # Set
+GITHUB_URL = "https://github.com/peterdesmet"
 
 # Blogroll
-LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
+LINKS = (
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
-DEFAULT_PAGINATION = False
+SOCIAL = (
+	("twitter", "https://www.twitter.com/peterdesmet"),
+	("github","https://github.com/peterdesmet"),
+	("linkedin","http://www.linkedin.com/in/peterdesmet"),
+	("lastfm","http://www.last.fm/user/anderhalv"),
+)
