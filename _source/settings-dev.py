8daf52384e48cb9ecdc198d7bd83c8be665f7f93
Peter Desmet <peter.desmet.work@gmail.com>
2013-03-13 13:44:08 +0100

    Add dev settings

--- _source/settings-dev.py
+++ _source/settings-dev.py
@@ -4,3 +4,12 @@
 import sys
 sys.path.append('.')
 from settings import *
+
+SITESUBTITLE = "Lucky rocketship underpants"
+
+# Reading settings
+DISPLAY_PAGES_ON_MENU = True
+DEFAULT_PAGINATION = 10 # To test pagination
+
+# External services
+GOOGLE_ANALYTICS = "UA-XXXXX-X" # Don't track while coding
